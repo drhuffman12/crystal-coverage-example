6c5e8625cc97fdc9045725ff13a8c5794a7d698f
Daniel Huffman <drhuffman12@gmail.com>
2021-06-14 01:48:00 -0400

    add scripts to run coverage tests in CI

---
+++ .github/workflows/coverage.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Minimum coverage percentage to pass:
+min=$1
+
+# File(s) to check:
+# f=$2
+# f=src/**/*.cr
+f=spec/**/*_spec.cr
+
+echo "\nTesting min coverage of $min % for files starting with $f\n"
+
+note_passed() {
+  echo "Passed at coverage percentage of $c; min is $min."
+  exit 0
+}
+note_failed() {
+  echo "Failed at coverage percentage of $c; min is $min."
+  exit 1
+}
+
+c=$(lib/coverage/bin/crystal-coverage $f | grep "covered" | grep -Eo '^[0-9]{1,2}')
+[ $c -ge $min ] && note_passed || note_failed
